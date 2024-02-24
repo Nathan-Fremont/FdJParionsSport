1c7aa3057690b0822f9ba2c9566b6ca10eba7c52
Nathan Frémont <nathan.fremontg@gmail.com>
2024-02-24 17:46:11 +0100

    Crete module coreTest

---
+++ coreTest/src/main/java/com/nathanfremont/coretest/CoroutineTestExtension.kt
@@ -0,0 +1,27 @@
+package com.nathanfremont.coretest
+
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.StandardTestDispatcher
+import kotlinx.coroutines.test.TestCoroutineScheduler
+import kotlinx.coroutines.test.TestDispatcher
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.setMain
+import org.junit.jupiter.api.extension.AfterEachCallback
+import org.junit.jupiter.api.extension.BeforeEachCallback
+import org.junit.jupiter.api.extension.ExtensionContext
+
+@OptIn(ExperimentalCoroutinesApi::class)
+class CoroutineTestExtension constructor(
+    private val dispatcher: TestDispatcher = StandardTestDispatcher(
+        scheduler = TestCoroutineScheduler()
+    )
+) : BeforeEachCallback, AfterEachCallback {
+    override fun beforeEach(context: ExtensionContext?) {
+        Dispatchers.setMain(dispatcher)
+    }
+
+    override fun afterEach(context: ExtensionContext?) {
+        Dispatchers.resetMain()
+    }
+}
